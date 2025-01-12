ad480b90af337da77ee10303e2a3cd9fa5066f81
CokdePutra <cokgdeputrawidnyana25@gmail.com>
2025-01-12 19:39:15 +0800

    replace index.html with index.php for redirect functionality

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Redirecting...</title>
+  <script>
+    // Redirect setelah 3 detik
+    setTimeout(function() {
+      window.location.href = "./home/homes.php";
+    }, 0);
+  </script>
+</head>
+
+<body></body>
+
+</html>
